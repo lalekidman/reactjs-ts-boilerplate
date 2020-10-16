c3b7824e7efc66f25dd9265a932c237bb3176345
Darryl Fabian <lalekidman@gmail.com>
2020-10-16 15:49:27 +0800

    done on base64 converting

--- src/controllers/main.tsx
+++ src/controllers/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import OrderComponent from './orders/order'
-import {Switch, withRouter} from 'react-router-dom'
-// import Toast from '../utils/Toast'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+// import {Switch, withRouter} from 'react-router-dom'
+// // import Toast from '../utils/Toast'
+// import {bindActionCreators} from 'redux'
+// import {connect} from 'react-redux'
 class Wrapper extends React.Component {
   render () {
     return (
